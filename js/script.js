c26a634c22ec85d1a76897276a182c5aa214509d
Julie <jq.nikitina@gmail.com>
2021-07-17 13:45:57 +0800

    Refactor: опечатка

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ let editForm = document.querySelector('.profile-form__form')
 
 let popup = document.querySelector('.profile-form');
 
-let currencyName = document.querySelector('.profile__name');
-let currencyDescription = document.querySelector('.profile__description');
+let currentName = document.querySelector('.profile__name');
+let currentDescription = document.querySelector('.profile__description');
 
 let fieldName = document.getElementById('profileName');
 let fieldDescription = document.getElementById('profileDescription');
 
 function popupOpen() {
   popup.classList.add('profile-form_active');
-  fieldName.value = currencyName.textContent;
-  fieldDescription.value = currencyDescription.textContent;
+  fieldName.value = currentName.textContent;
+  fieldDescription.value = currentDescription.textContent;
 }
 
 function popupClose() {
@@ -23,8 +23,8 @@ function popupClose() {
 
 function changeName(evt) {
   evt.preventDefault();
-  currencyName.textContent = fieldName.value;
-  currencyDescription.textContent = fieldDescription.value;
+  currentName.textContent = fieldName.value;
+  currentDescription.textContent = fieldDescription.value;
   popupClose();
 }
 
