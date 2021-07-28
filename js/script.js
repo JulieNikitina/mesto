7d5107fe98d52d01ba9fc71e66f705054555f75f
Julie <jq.nikitina@gmail.com>
2021-07-29 00:27:57 +0800

    Add initial cards by js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Работа с формой редактирования профиля
 let closeButton = document.querySelector('.profile-form__toggle');
 let editButton = document.querySelector('.profile__edit-button');
 
@@ -10,6 +11,46 @@ let currentDescription = document.querySelector('.profile__description');
 
 let fieldName = document.getElementById('profileName');
 let fieldDescription = document.getElementById('profileDescription');
+
+// Работа с выводом карточек элементов
+const elementsContainer = document.querySelector('.elements');
+const elementTemplate = document.querySelector('#element-template').content;
+
+const initialCards = [
+  {
+    name: 'Ольхон',
+    link: './images/elements/elements-olkhon.JPG'
+  },
+  {
+    name: 'Бухта Ая',
+    link: './images/elements/elements-aya.JPG'
+  },
+  {
+    name: 'Тажеранские степи',
+    link: './images/elements/elements-tazherany.JPG'
+  },
+  {
+    name: 'Мамай',
+    link: './images/elements/elements-mamay.JPG'
+  },
+  {
+    name: 'Большая Байкальская Тропа',
+    link: './images/elements/elements-bbt.JPG'
+  },
+  {
+    name: 'Аршан',
+    link: './images/elements/elements-arshan.JPG'
+  }
+];
+
+initialCards.forEach(function (element) {
+  const elementCard = elementTemplate.querySelector('.element').cloneNode(true);
+  elementCard.querySelector('.element__title').textContent = element.name;
+  elementCard.querySelector('.element__photo').src = element.link;
+  elementCard.querySelector('.element__photo').alt = element.name;
+  elementsContainer.append(elementCard);
+}
+)
 
 function openPopup() {
   popup.classList.add('profile-form_active');
