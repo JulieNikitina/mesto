e7a9c422c3a909163319dfd4c40ef77d9ba9cde8
Julie <jq.nikitina@gmail.com>
2021-07-29 15:04:33 +0800

    Add open/close second popup

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 // Работа с формой редактирования профиля
-let closeButton = document.querySelector('.profile-form__toggle');
+
 let editButton = document.querySelector('.profile__edit-button');
+let addButton = document.querySelector('.profile__add-button');
 
-let editForm = document.querySelector('.profile-form__form')
 
-let popup = document.querySelector('.profile-form');
+let editForm = document.querySelector('.form__form')
+
+
+// находим нужные попапы
+// попап редактирования
+let popupEdit = document.getElementById('profileForm');
+// кнопка закрытия в попапе
+let closeEditFormButton = popupEdit.querySelector('.form__toggle');
+// попап добавления
+let popupAdd = document.getElementById('addCardForm');
+// кнопка закрытия в попапе
+let closeAddFormButton = popupAdd.querySelector('.form__toggle');
+
 
 let currentName = document.querySelector('.profile__name');
 let currentDescription = document.querySelector('.profile__description');
@@ -12,54 +24,18 @@ let currentDescription = document.querySelector('.profile__description');
 let fieldName = document.getElementById('profileName');
 let fieldDescription = document.getElementById('profileDescription');
 
-// Работа с выводом карточек элементов
-const elementsContainer = document.querySelector('.elements');
-const elementTemplate = document.querySelector('#element-template').content;
 
-const initialCards = [
-  {
-    name: 'Ольхон',
-    link: './images/elements/elements-olkhon.JPG'
-  },
-  {
-    name: 'Бухта Ая',
-    link: './images/elements/elements-aya.JPG'
-  },
-  {
-    name: 'Тажеранские степи',
-    link: './images/elements/elements-tazherany.JPG'
-  },
-  {
-    name: 'Мамай',
-    link: './images/elements/elements-mamay.JPG'
-  },
-  {
-    name: 'Большая Байкальская Тропа',
-    link: './images/elements/elements-bbt.JPG'
-  },
-  {
-    name: 'Аршан',
-    link: './images/elements/elements-arshan.JPG'
-  }
-];
 
-initialCards.forEach(function (element) {
-  const elementCard = elementTemplate.querySelector('.element').cloneNode(true);
-  elementCard.querySelector('.element__title').textContent = element.name;
-  elementCard.querySelector('.element__photo').src = element.link;
-  elementCard.querySelector('.element__photo').alt = element.name;
-  elementsContainer.append(elementCard);
-}
-)
-
-function openPopup() {
-  popup.classList.add('profile-form_active');
+function openPopup(popup) {
+  popup.classList.add('form_active');
   fieldName.value = currentName.textContent;
   fieldDescription.value = currentDescription.textContent;
 }
 
-function closePopup() {
-  popup.classList.remove('profile-form_active');
+function closePopup(event) {
+  const popup = event.target.closest('.form');
+  console.log(popup)
+  popup.classList.remove('form_active');
 }
 
 function changeName(evt) {
@@ -70,7 +46,12 @@ function changeName(evt) {
 }
 
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', function() {openPopup(popupEdit)});
+addButton.addEventListener('click', function() {openPopup(popupAdd)});
+
+closeEditFormButton.addEventListener('click', closePopup);
+closeAddFormButton.addEventListener('click', closePopup);
+
 editForm.addEventListener('submit', changeName);
+
 
